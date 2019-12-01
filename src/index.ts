95b9a068b1f17a8e2fd8d4b2189468559f54774e
Cole Kissane <coler706@gmail.com>
2019-12-01 12:03:33 -0800

    Better Harmonic Line Detection

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,15 @@ import reglCreate from 'regl';
 const regl=reglCreate({extensions:['webgl_draw_buffers', 'oes_texture_float','oes_texture_float_linear']});
 // const regl = require('regl')({extensions:["OES_texture_float"]})
 const mouse = require('mouse-change')()
-var subsamp=4096;
+var subsamp=4096*2;
 var mc=261.625565;
 const gSize=128;
 var simF=mc*gSize;
+var samN=0;
 var stepsT=0;
 var stepsQ=0;
 var lastSTm=new Date().getTime();
+var lastFm=new Date().getTime();
 // const iterateBounce = gpu.createKernel(function(a: number[][][]) {
 //   let sum = 0;
 //   if(this.thread.z==0){
@@ -76,8 +78,10 @@ require('getusermedia')({audio: true}, function (err, stream) {
     var ave=vu.reduce((a,b)=>a+b,0)/vu.length;
     var nv=[];
     for(var i=0;i<vu.length;i++){
-nv[i]=Math.abs(vu[i])<0.0?0:vu[i];
-    }
+      
+nv[i]=vu[i];//Math.sin(Math.PI*2*samN/context.sampleRate*mc)*0.5;//Math.abs(vu[i])<0.0?0:vu[i];
+samN+=1;    
+}
     stepsT=(((stepsT-vu.length/context.sampleRate)%(1/mc)+(1/mc))%(1/mc));
     window.vol=nv;//vol.concat(vu);//ave*1024;//(ave>0?vu.reduce((a,b)=>Math.max(a,b),0):vu.reduce((a,b)=>Math.min(a,b),0))/2+ave/2;///vu.length*128.0;
     
@@ -93,7 +97,8 @@ nv[i]=Math.abs(vu[i])<0.0?0:vu[i];
   }
   function getVolS(){
     var rn=stepsT*context.sampleRate;
-      return window.vol[Math.floor(rn%subsamp)]||0
+    var ler=rn%1;
+      return (window.vol[Math.floor(rn%subsamp)]||0)*(1-ler)+(window.vol[Math.floor((rn+1)%subsamp)]||0)*ler;
   }
   // source.start();
   
@@ -163,14 +168,17 @@ uniform float t;
 uniform float vol;
 varying vec2 uv;
 void main () {
-    float mp=1.0;
+    float mp=1.0;///2.0;
     
     vec2 cVa=texture2D(texture,uv).xy;
+    float sX=cVa.x+0.0;
+    float gn=texture2D(texture,uv).z;
     cVa.y=cVa.y*2.0-1.0;
     
     float q=0.0;
     float qC=0.0;
-    for(int i=1;i<2;i++){
+    int i=1;
+   // for(int i=1;i<2;i++){
       float s=float(i);
       float c=1.0/(s);
     vec2 up=uv+vec2(0.0,-1.0)/res.xy*s;
@@ -191,27 +199,52 @@ void main () {
     q+=(lVa.x)*c;
     q+=(rVa.x)*c;
     qC+=c*4.0;
-    }
-    cVa.y+=q/qC-cVa.x;
+    //}
+    float pos=cVa.x-0.5;
+    
+    float accel=q/qC-cVa.x;
+    cVa.y+=accel/2.0;
+    float vel=cVa.y+0.0;
+    cVa.y+=accel/2.0;
     //cVa.x+=cVa.y*mp;
     //uVa.x+=uVa.y*mp;
     //lVa.x+=lVa.y*mp;
     //rVa.x+=rVa.y*mp;
     //dVa.x+=dVa.y*mp;
     
-    
-    cVa=vec2(0.5,0.0)*0.001+cVa*0.999;
+    float gnt=0.0;
+   
     if(t<20.0){
         cVa=vec2(0.5,0.0);
+        //gn=0.0;
     }
-    
+    gnt=gn;//1.0;//abs(abs(rVa.x-0.5)+abs(lVa.x-0.5)+abs(uVa.x-0.5)+abs(dVa.x-0.5)-abs((rVa.x-0.5)+(lVa.x-0.5)+(uVa.x-0.5)+(dVa.x-0.5))*2.0)+abs(cVa.x-0.5);
+    float apRat=pow(abs(-accel/pos),0.5);
+    gnt=pow(pow(pos,2.0)+pow(vel,2.0)/abs(-accel/pos),0.5);
+   
     cVa.x+=cVa.y*mp;
-    float j=1.0-length(abs(uv-vec2(0.5))*res.xy)/2.0;//pow(2.0,-pow(length(abs(uv-vec2(0.5))*res.xy)/10.0,2.0));
+    //cVa.y=(vec2(0.5,0.0)*0.001+cVa*0.999).y;
+    float j=1.0-length(floor(abs(uv-vec2(0.5))*res.xy-vec2(0.5)));//pow(2.0,-pow(length(abs(uv-vec2(0.5))*res.xy)/10.0,2.0));
+    //j=1.0-length(floor(abs(uv)*res.xy));//pow(2.0,-pow(length(abs(uv-vec2(0.5))*res.xy)/10.0,2.0));
+    
     if( j>0.0 ){
-      cVa=cVa*(1.0-j)+vec2(vol+0.5,0.0)*j;//cos(t*0.1*2.0*atan(0.0,-1.0))/4.0+0.5;
+    
+      cVa.x=cVa.x*(1.0-j)+vec2(vol/2.0+0.5,0.0).x*j;//cos(t*0.1*2.0*atan(0.0,-1.0))/4.0+0.5;
       //cVa.y=0.0;//-2.0*atan(0.0,-1.0)*0.01*sin(t*0.01*2.0*atan(0.0,-1.0))/2.0+0.5;
     }
-  gl_FragColor = vec4(vec3(cVa.x,cVa.y*0.5+0.5,1.0),1.0);
+    if(t<20.0){
+      gn=0.5;
+    }
+    if(gnt<1.0){
+      gn=abs(gnt*0.001+gn*0.999);
+    }
+    
+    
+    if(t<20.0){
+      gn=0.5;
+    }
+    cVa=(vec2(0.5,0.0)*0.001+cVa*0.999);
+  gl_FragColor = vec4(vec3(cVa.x,cVa.y*0.5+0.5,gn),1.0);
 }`;
 const drawFeedback = regl({
   frag: fr,
@@ -352,7 +385,7 @@ const drawNi = regl({
   
     void main () {
         float mp=1.0;
-        float qb=1.0;
+        float qb=1.5;
         vec2 rc=vec2(min(res.x,res.y));
         vec2 uv2=(uv-vec2(0.5,0.5)-vec2(0.5,0.5)/res.xy)*res.xy/rc.xy+vec2(0.5,0.5);
         vec2 up=uv2+vec2(0.0,-1.0)/res.xy*qb;
@@ -360,6 +393,8 @@ const drawNi = regl({
         vec2 right=uv2+vec2(1.0,0.0)/res.xy*qb;
         vec2 down=uv2+vec2(0.0,1.0)/res.xy*qb;
         vec2 cVa=texture2D(texture,uv2).xy;
+        float gn=texture2D(texture,uv2).z;
+        float gnB=texture2D(texture,vec2(0.0,0.0)).z;
         cVa.y=cVa.y*2.0-1.0;
         vec2 uVa=texture2D(texture,up).xy;
         uVa.y=uVa.y*2.0-1.0;
@@ -370,6 +405,7 @@ const drawNi = regl({
         vec2 dVa=texture2D(texture,down).xy;
         dVa.y=dVa.y*2.0-1.0;
         vec2 aa=(cVa+uVa+dVa+lVa+rVa)/5.0;
+        
         //cVa.x+=cVa.y*mp;
         //uVa.x+=uVa.y*mp;
         //lVa.x+=lVa.y*mp;
@@ -377,13 +413,34 @@ const drawNi = regl({
         //dVa.x+=dVa.y*mp;
         vec3 normal=vec3(0.0,0.0,0.0);
         float h=100.0;
+        // gn=1.0;
+        // if(abs(cVa.y)+abs(cVa.x-0.5)<abs(dVa.x-0.5)+abs(dVa.y)){
+        //   gn+=-0.25;
+        // }
+        // if(abs(cVa.y)+abs(cVa.x-0.5)<abs(uVa.x-0.5)+abs(uVa.y)){
+        //   gn+=-0.25;
+        // }
+        // if(abs(cVa.y)+abs(cVa.x-0.5)<abs(lVa.x-0.5)+abs(lVa.y)){
+        //   gn+=-0.25;
+        // }
+        // if(abs(cVa.y)+abs(cVa.x-0.5)<abs(rVa.y)+abs(rVa.x-0.5)){
+        //   gn+=-0.25;
+        // }
+        
+        
+        
+        
         normal+=normalize(vec3(cVa.x-rVa.x,0.0,1.0/h));
         normal+=normalize(-vec3(cVa.x-lVa.x,0.0,-1.0/h));
         normal+=normalize(vec3(0.0,cVa.x-dVa.x,1.0/h));
         normal+=normalize(-vec3(0.0,cVa.x-uVa.x,-1.0/h));
         normal=normalize(normal);
         float colA=atan(normal.y,normal.x)/atan(0.0,-1.0)/2.0;
-        vec3 col=hsl2rgb(vec3(mod(colA*2.0,1.0),1.0,(0.1-abs(aa.x-0.5))*10.0*length(normal.xy)));
+        if(cVa.y<0.0){
+          //colA+=0.5;
+          colA=mod(colA,1.0);
+        }
+        vec3 col=hsl2rgb(vec3(mod(colA*2.0,1.0),1.0,min(max(0.5-gn/gnB,0.0)*2.0,1.0)));//max(1.0-10.0*pow(pow((cVa.x-0.5)*4.0,2.0)+pow(cVa.y*8.0,2.0),0.5),0.0)));
       gl_FragColor = vec4(vec3(dot(normal,normalize(vec3(-1.0,2.0,1.0)))*0.5)*col*0.0+col,1.0);
       vec2 uvc=(uv2-vec2(0.5,0.5))*2.0;
       if(max(abs(uvc.x),abs(uvc.y))>1.0){
@@ -432,17 +489,39 @@ regl.frame(function () {
 //   regl.clear({
 //     color: [0.5, 0.5, 0.5, 1]
 //   })
-for(var i=0;i<1000;i++){
+var tm=new Date().getTime();
+while(lastSTm<tm){
+  lastSTm+=1000/simF;
   stepsT+=1/mc/gSize;
   stepsQ+=1;
-  var tm=new Date().getTime();
-  if(tm-lastSTm>1000){
-   console.log("sps",stepsQ,"need",mc*gSize,context.sampleRate/(mc*gSize))
-    lastSTm=tm;
-    stepsQ=0;
-  }
+  // if(tm-lastSTm>1000){
+  //  console.log("sps",stepsQ,"need",mc*gSize,context.sampleRate/(mc*gSize))
+  //   lastSTm=tm;
+  //   stepsQ=0;
+  // }
   (flips++)%2===0?drawFeedback():drawFeedback2();
+  if(new Date().getTime()-tm>1000/60){
+    break;
+  }
 }
+var tm=new Date().getTime();
+//   if(tm-lastFm>1000){
+//    console.log("sps",stepsQ,"need",mc*gSize,context.sampleRate/(mc*gSize))
+//     lastFm=tm;
+//     stepsQ=0;
+//   }
+lastSTm=tm;
+// for(var i=0;i<128;i++){
+//   stepsT+=1/mc/gSize;
+//   stepsQ+=1;
+//   var tm=new Date().getTime();
+//   if(tm-lastSTm>1000){
+//    console.log("sps",stepsQ,"need",mc*gSize,context.sampleRate/(mc*gSize))
+//     lastSTm=tm;
+//     stepsQ=0;
+//   }
+//   (flips++)%2===0?drawFeedback():drawFeedback2();
+// }
 drawNi();
 //   pixels({
 //     copy: true

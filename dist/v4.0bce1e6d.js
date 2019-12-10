889b7e483b4b442f2b3c82418ed5b06e5b360ed4
Cole Kissane <coler706@gmail.com>
2019-12-09 17:16:02 -0800

    YES

--- dist/v4.0bce1e6d.js
+++ dist/v4.0bce1e6d.js
@@ -39924,7 +39924,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45543" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37749" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
